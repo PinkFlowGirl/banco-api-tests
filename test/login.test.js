afc1c824abdca7c681470bf454b3db9c37a93fd5
Claudia Mangueira <claudiaoliveiramangueira@gmail.com>
2026-03-25 08:04:55 -0300

    Adicionando a biblioteca .env para variaveris globais

--- test/login.test.js
+++ test/login.test.js
@@ -1,10 +1,11 @@
 const request = require('supertest'); //Importa a biblioteca supertest para fazer requisições HTTP
 const { expect } = require('chai'); //Importa a função expect da biblioteca chai para fazer asserções nos testes
+require('dotenv').config()
 
 describe('Login', () => { //Inicia a descrição do conjunto de testes para a funcionalidade de login
     describe('POST /login', () => { //Inicia a descrição do teste para a rota POST /login
         it('Deve retornar 200 com un token em string quando usar credenciais válidas', async () => { //Define o teste que espera um status 200 e um token em string ao usar credenciais válidas
-            const resposta = await request('http://localhost:3000') //Faz uma requisição HTTP para o servidor local na porta 3000
+            const resposta = await request(process.env.BASE_URL) // requisição HTTP para o servidor local na porta 3000
                 .post('/login') //Especifica que a requisição é do tipo POST para a rota /login
                 .set('Content-Type', 'application/json') //Define o cabeçalho da requisição para indicar que o corpo da requisição é do tipo JSON
                 .send({ //Envia o corpo da requisição com as credenciais de login
--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,10 +1,11 @@
 const request = require('supertest'); //Importa a biblioteca supertest para fazer requisições HTTP
 const { expect } = require('chai'); //Importa a função expect da biblioteca chai para fazer asserções nos testes
+require('dotenv').config()
 
 describe('Transferencias', () => {
     describe('POST /transferencias', () => {
         it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de R$ 10,00', async () => {
-            const respostaLogin = await request('http://localhost:3000') //Faz uma requisição HTTP para o servidor local na porta 3000
+            const respostaLogin = await request(process.env.BASE_URL) // requisição HTTP para o servidor local na porta 3000
                 .post('/login') //Especifica que a requisição é do tipo POST para a rota /login
                 .set('Content-Type', 'application/json') //Define o cabeçalho da requisição para indicar que o corpo da requisição é do tipo JSON
                 .send({ //Envia o corpo da requisição com as credenciais de login
@@ -14,7 +15,7 @@ describe('Transferencias', () => {
 
             const token = respostaLogin.body.token // Armazena o token retornado no corpo da resposta do login
 
-            const resposta = await request('http://localhost:3000') //Faz uma requisição HTTP para o servidor local na porta 3000
+            const resposta = await request(process.env.BASE_URl) // HTTP para o servidor local na porta 3000
                 .post('/transferencias') //Especifica que a requisição é do tipo POST para a rota /transferencias
                 .set('Content-Type', 'application/json') //Define o cabeçalho da requisição para indicar que o corpo da requisição é do tipo JSON
                 .set('Authorization', `Bearer ${token}`) // 
@@ -25,25 +26,25 @@ describe('Transferencias', () => {
                     token: ""
                 })
 
-            expect(resposta.status).to.equal(201); 
-            console.log(resposta.body) 
+            expect(resposta.status).to.equal(201);
+            console.log(resposta.body)
         })
 
         it('Deve retornar falha com 422 quando o valor da transferencia for abaixo de R$ 10,00', async () => {
-            const respostaLogin = await request('http://localhost:3000') 
-                .post('/login') 
-                .set('Content-Type', 'application/json') 
-                .send({ 
-                    "username": 'julio.lima', 
-                    "senha": '123456' 
+            const respostaLogin = await request('http://localhost:3000')
+                .post('/login')
+                .set('Content-Type', 'application/json')
+                .send({
+                    "username": 'julio.lima',
+                    "senha": '123456'
                 })
-                
-            const token = respostaLogin.body.token 
 
-            const resposta = await request('http://localhost:3000') 
-                .post('/transferencias') 
-                .set('Content-Type', 'application/json') 
-                .set('Authorization', `Bearer ${token}`) 
+            const token = respostaLogin.body.token
+
+            const resposta = await request('http://localhost:3000')
+                .post('/transferencias')
+                .set('Content-Type', 'application/json')
+                .set('Authorization', `Bearer ${token}`)
                 .send({
                     contaOrigem: 1,
                     contaDestino: 2,
