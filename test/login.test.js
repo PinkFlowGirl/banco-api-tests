0585e99ab27287a79f1955456e37cfe37db50a32
Claudia Mangueira <claudiaoliveiramangueira@gmail.com>
2026-03-25 22:21:21 -0300

    Adicionando fixtures para gerir os corpos das requisições

--- test/login.test.js
+++ test/login.test.js
@@ -1,17 +1,17 @@
 const request = require('supertest'); //Importa a biblioteca supertest para fazer requisições HTTP
 const { expect } = require('chai'); //Importa a função expect da biblioteca chai para fazer asserções nos testes
 require('dotenv').config()
+const postLogin = require('../fixtures/postLogin.json') //Importa os dados de login do arquivo postLogin.json
 
-describe('Login', () => { //Inicia a descrição do conjunto de testes para a funcionalidade de login
-    describe('POST /login', () => { //Inicia a descrição do teste para a rota POST /login
-        it('Deve retornar 200 com un token em string quando usar credenciais válidas', async () => { //Define o teste que espera um status 200 e um token em string ao usar credenciais válidas
-            const resposta = await request(process.env.BASE_URL) // requisição HTTP para o servidor local na porta 3000
-                .post('/login') //Especifica que a requisição é do tipo POST para a rota /login
-                .set('Content-Type', 'application/json') //Define o cabeçalho da requisição para indicar que o corpo da requisição é do tipo JSON
-                .send({ //Envia o corpo da requisição com as credenciais de login
-                    "username": 'julio.lima', //Define o nome de usuário para login
-                    "senha": '123456' //Define a senha para login
-                })
+describe('Login', () => { 
+    describe('POST /login', () => { 
+        it('Deve retornar 200 com un token em string quando usar credenciais válidas', async () => { 
+            const bodyLogin = { ...postLogin } 
+            
+            const resposta = await request(process.env.BASE_URL) 
+                .post('/login') 
+                .set('Content-Type', 'application/json') 
+                .send(bodyLogin)
 
 
             expect(resposta.status).to.equal(200); //Verifica se o status da resposta é igual a 200, indicando sucesso
--- test/transferencia.test.js
+++ test/transferencia.test.js
@@ -1,7 +1,9 @@
 const request = require('supertest'); //Importa a biblioteca supertest para fazer requisições HTTP
 const { expect } = require('chai'); //Importa a função expect da biblioteca chai para fazer asserções nos testes
 require('dotenv').config()
+
 const { obterToken } = require('../helpers/autenticacao')
+const postTransferencias = require('../fixtures/postTransferencias.json')    
 
 describe('Transferencias', () => {
     describe('POST /transferencias', () => {
@@ -11,35 +13,28 @@ describe('Transferencias', () => {
             token = await obterToken('julio.lima', '123456')
         })
         it('Deve retornar sucesso com 201 quando o valor da transferencia for igual ou acima de R$ 10,00', async () => {
-            
+            const bodyTransferencias = { ...postTransferencias }
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    contaOrigem: 1,
-                    contaDestino: 2,
-                    valor: 11,
-                    token: ""
-                })
-
+                .send(bodyTransferencias)
+                
             expect(resposta.status).to.equal(201);
             console.log(resposta.body)
         })
 
         it('Deve retornar falha com 422 quando o valor da transferencia for abaixo de R$ 10,00', async () => {
+           const bodyTransferencias = { ...postTransferencias }
+           bodyTransferencias.valor = 7
+
             const respostaLogin = await request(process.env.BASE_URL)
              
             const resposta = await request(process.env.BASE_URL)
                 .post('/transferencias')
                 .set('Content-Type', 'application/json')
                 .set('Authorization', `Bearer ${token}`)
-                .send({
-                    contaOrigem: 1,
-                    contaDestino: 2,
-                    valor: 7,
-                    token: ""
-                })
+                .send(bodyTransferencias)
 
             expect(resposta.status).to.equal(422);
         })
